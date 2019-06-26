0f1a1ad6d29c82dcd3fa0761acee76fd63231583
AsheToll <github email address>
2019-06-26 18:26:24 -0400

    FINISHED.

--- lib/person.rb
+++ lib/person.rb
@@ -76,11 +76,58 @@ class Person
         self.bank_account += salary
         return "all about the benjamins"
     end
-    # The take_bath method should increment the person's hygiene points by four and return the string "♪ Rub-a-dub just relaxing in the tub ♫".
+
     def take_bath
         # take hygiene and increase by 4
         take_bath_increase = self.hygiene + 4
         self.hygiene = take_bath_increase
         return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        # increment happiness by 2
+        happiness_increase = self.happiness + 2
+        self.happiness = happiness_increase
+        # decrease hygiene by 3
+        hygiene_decrease = self.hygiene - 3
+        self.hygiene = hygiene_decrease
+        # return Queen lyrics
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person)
+        # makes the person calling happier by three points
+        happier_person = self.happiness + 3
+        self.happiness = happier_person
+
+        happier_friend = person.happiness + 3
+        person.happiness = happier_friend
+        # incrementing both happiness points by 3
+        # If Stella calls Felix, return "Hi Felix! It's Stella. How are you?"
+        return "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+    
+    # accept two arguments, person and topic
+    def start_conversation(person, topic)
+        # If the topic is politics, both people get sadder and the method returns "blah blah partisan blah lobbyist".
+        if topic == 'politics'
+            sadder_person = self.happiness - 2
+            self.happiness = sadder_person
+
+            sadder_friend = person.happiness - 2
+            person.happiness = sadder_friend
+            return "blah blah partisan blah lobbyist"
+        # If the topic is weather, both people get a little happier and the method returns "blah blah sun blah rain".
+        elsif topic == 'weather'
+            happier_person = self.happiness + 1
+            self.happiness = happier_person
+
+            happier_friend = person.happiness + 1
+            person.happiness = happier_friend
+            return "blah blah sun blah rain"
+        # If the topic is not politics or weather, their happiness points don't change and the method returns "blah blah blah blah blah".
+        else
+            return "blah blah blah blah blah"
+        end
     end
 end
